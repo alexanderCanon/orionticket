b58394ad96a384e52be20a64f9bc4e21e0d28266
alexanderCanon <alexandercanon64@gmail.com>
2026-05-21 17:43:04 -0600

    removing old *ps1 files to avoid Powershell/Linux environment confussion. Att Alex, not AI ;)

--- remove_bom_all.ps1
+++
@@ -1,6 +0,0 @@
-Get-ChildItem -Path . -Include *.java,*.xml -Recurse | ForEach-Object {
-    $content = [System.IO.File]::ReadAllText($_.FullName)
-    $utf8NoBom = New-Object System.Text.UTF8Encoding $false
-    [System.IO.File]::WriteAllText($_.FullName, $content, $utf8NoBom)
-}
-Write-Host "Removed BOM from all .java and .xml files."
