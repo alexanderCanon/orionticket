a198a5cbf6753af3bfcc170a12e0e2d76c714e57
alexanderCanon <alexandercanon64@gmail.com>
2026-05-23 00:09:46 -0600

    updates

--- event-management-service/src/main/java/com/orionticket/events/infrastructure/config/SecurityConfig.java
+++ event-management-service/src/main/java/com/orionticket/events/infrastructure/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -27,6 +28,11 @@ import java.util.Objects;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers("/v1/catalog/**");
+    }
 
     @Bean
     @Order(0)
